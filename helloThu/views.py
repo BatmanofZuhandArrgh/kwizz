519c62953ad7e4f2267b4d91c9ba57f9a9c9f547
BatmanofZuhandArrgh <anhnguyen199819981998@gmail.com>
2021-05-10 17:11:15 +0700

    test commit

--- helloThu/views.py
+++ helloThu/views.py
@@ -2,4 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def myView(request):
+    print('whoa')
     return HttpResponse('Hello, Thu. I love you. You are my World')
